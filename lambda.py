e5fd5ef287dfd317b9ea2e1ac6f192202873bfb0
RedAmashi <sildar657@mail.ru>
2025-02-07 08:51:13 +0300

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,51 +1,25 @@
-def simple_prime_generator(limit):
-    
-    is_prime = [True] * limit
+from functools import reduce
 
 
-    if limit > 0:
-        is_prime[0] = False
-    if limit > 1:
-         is_prime[1] = False
-
-    
-    for number in range(2, limit):
-        
-        if is_prime[number]:
-            yield number
-
-            
-            for multiple in range(number + number, limit, number):
-                is_prime[multiple] = False
+def square_numbers(numbers: list) -> list:
+    return list(map(lambda x: x ** 2, numbers))
 
 
-# Пример использования
-for prime in simple_prime_generator(15):
-    print(prime) 
+def filter_even_numbers(numbers: list) -> list:
+    return filter(lambda x: x % 2 == 0, numbers)
 
 
-class SimpleCyclicIterator:
+def multiply_numbers(numbers: list) -> int:
+    return reduce(lambda x, y: x * y, numbers)
 
 
-    def __init__(self, items):
-       
-        
-        self.items = items # Список для перебора
-        self.index = 0 # Начальный индекс
-        self.length = len(items) # Длина списка
-
-    def __iter__(self):
-       
-        return self
-
-    def __next__(self):
-        
-        current = self.items[self.index]  # Берем текущий элемент
-        self.index = (self.index + 1) % self.length # Обновляем индекс (делаем круг)
-        return current # Возвращаем элемент
+def sum_of_squares_of_even_numbers(numbers: list) -> int:
+    numbers = filter(lambda x: x % 2 == 0, numbers)
+    numbers = list(map(lambda x: x ** 2, numbers))
+    return reduce(lambda x, y: x + y, numbers)
 
 
-# Пример использования
-it = SimpleCyclicIterator([1, 2, 3])
-for _ in range(7):
-    print(next(it))
+def word_length_map(input_string):
+    dictionary = dict()
+    a = any(map(lambda s: dictionary.update({s: len(s)}), input_string.split()))
+    return dictionary
